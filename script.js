bdd041638cf1351781477e4638c3cba37bece049
Annie Huang <annie.hn@gmail.com>
2024-01-01 15:09:59 +1100

    Change hero-title__overlay to hero-title so we can use .hero-title to style both text on h1 and <p>

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const heroTitle = document.querySelector('.hero-title');
-const heroTitleOverlay = document.querySelector('.hero-title__overlay');
+const heroTitleOverlay = document.querySelector('.hero-title[data-overlay]');
 
 heroTitleOverlay.innerText = heroTitle.innerText;
 
